b2262f59b4784af5a974dc8c4035477f36761917
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-19 15:12:23 +0000

    feat: create ui for login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,3 +1,98 @@
+"use client";
+
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons/Icons";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link"
+
+
 export default function Login() {
-  return <h1>Login</h1>;
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center px-5">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        XY Forms
+      </Link>
+      <form onSubmit={onSubmit} className="flex flex-col gap-5">
+        <center>
+          <h1 className="text-2xl font-semibold tracking-tight mb-3">
+            Login to your account
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Enter your email and password below
+          </p>
+        </center>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              placeholder="password"
+              type="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              disabled={isLoading}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Login
+          </Button>
+          <div className="relative mt-2">
+            <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+                Or 
+            </span>
+            </div>
+        </div>
+        <Button variant="outline" type="button" disabled={isLoading}>
+            <Link href="/auth/register">
+                register
+            </Link>
+        </Button>
+        
+        </div>
+      </form>
+    </div>
+  );
 }
