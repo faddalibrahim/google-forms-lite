468634468073199462066ebf6859288af28375fc
Faddal Ibrahim <faddalibrahim@gmail.com>
2024-01-22 10:37:06 +0000

    feat: move form name to second row on mobile screens

--- src/components/custom/Appbar.tsx
+++ src/components/custom/Appbar.tsx
@@ -4,27 +4,27 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Appbar() {
   return (
-    <div className="flex justify-between items-center w-screen p-5 bg-white gap-4">
-      <div className="flex flex-col md:flex-row gap-5 items-center">
+    <div className="grid grid-cols-3 gap-2 justify-between w-screen py-3 px-3 md:px-5 bg-white">
+      <div className="flex flex-col md:flex-row gap-3 items-center justify-self-start">
         <Icons.logo className="" />
-        <h1>Untitled Form</h1>
+        <h1 className="hidden lg:block">Untitled Form</h1>
       </div>
-      <div className="flex gap-7 items-center">
-        <div className="flex gap-3">
-          <Icons.undo />
+      <div className="flex gap-7 items-center justify-self-center">
+        <div className="flex gap-5">
+          <Icons.undo className="" />
           <Icons.redo />
           <Icons.preview />
         </div>
-        <div>
-          <Button variant="default">Save</Button>
-        </div>
       </div>
-      <div>
+      <div className="justify-self-end">
         <Avatar>
           <AvatarImage src={`/img/hello.png`} alt="@shadcn" />
           <AvatarFallback>US</AvatarFallback>
         </Avatar>
       </div>
+      <h1 className="lg:hidden sm:block col-span-3 text-center">
+        Untitled Form
+      </h1>
     </div>
   );
 }
